f6d2615f5d51ec7b795cfc2a05346abdecb6fbdf
Arne Hilmann <arne@hilmann.de>
2013-04-10 20:19:10 +0200

    renaming of some functions, spawn_link for both listeners, fewer error handling

--- src/tail2net.erl
+++ src/tail2net.erl
@@ -10,41 +10,40 @@
 -define(FILE_POLLINTERVALL, 1000).
 
 run() ->
-    register(filelistener, spawn(fun() -> listen_on_file() end)),
-    register(portlistener, spawn(fun() -> listen_on_port() end)),
-    portlistener ! spawn,
+    register(filelistener, spawn_link(fun() -> open_file() end)),
+    register(portlistener, spawn_link(fun() -> open_port() end)),
+    portlistener ! accept_connection,
     % TODO how to do it better than this?!
     receive
         _ -> ok
     end.
 
-listen_on_file() ->
+open_file() ->
     case file:open(?FILE2TAIL, [read, raw, read_ahead]) of
         {ok, IoDevice} -> 
             listen_on_file(IoDevice);
-        % TODO real error handling!
-        {error, Reason} ->
-            io:format("cannot open file: ~p\n", [Reason])
+        {error, enoent} ->
+            % TODO extract function, see line 44 also
+            receive
+            after ?FILE_POLLINTERVALL ->
+                open_file()
+            end
     end.
 
 listen_on_file(IoDevice) ->
     case file:read_line(IoDevice) of
         {ok, Data} ->
-            io:format("new data: ~p\n", [Data]),
+            io:format("~s", [Data]),
             portlistener ! {broadcast, Data},
             listen_on_file(IoDevice);
         eof ->
             receive
             after ?FILE_POLLINTERVALL ->
                 listen_on_file(IoDevice)
-            end;
-        % TODO real error handling! 
-        {error, Reason} ->
-            io:format("error: ~p\n", [Reason]),
-            coordinator ! finished
+            end
     end.
 
-listen_on_port() ->
+open_port() ->
     io:format("trying to listen on port ~B\n", [?PORT]),
     {ok, LSocket} = gen_tcp:listen(?PORT, ?TCP_OPTIONS),
     process_flag(trap_exit, true),
@@ -52,25 +51,22 @@ listen_on_port() ->
 
 listen_on_port(LSocket, Worker) ->
     receive
-        spawn ->
-            Pid = spawn(fun() -> accept(LSocket) end),
+        accept_connection ->
+            Pid = spawn(fun() -> accept_connection(LSocket) end),
             link(Pid),
             listen_on_port(LSocket, [Pid|Worker]);
         {broadcast, Data} ->
             [Kid ! {broadcast, Data} || Kid  <- Worker ],
             listen_on_port(LSocket, Worker);
         {'EXIT', SomePid, normal} ->
-            listen_on_port(LSocket, lists:delete(SomePid, Worker));
-        {'EXIT', SomePid, Reason} ->
-            io:format("child with pid ~p exited, reason ~p\n", [SomePid, Reason]),
             listen_on_port(LSocket, lists:delete(SomePid, Worker))
     end.
 
-accept(LSocket) ->
+accept_connection(LSocket) ->
     io:format("Socket ~p waiting for connections\n", [LSocket]),
     case gen_tcp:accept(LSocket) of
         {ok, Socket} ->
-            portlistener ! spawn,
+            portlistener ! accept_connection,
             gen_tcp:controlling_process(Socket, self()),
             io:format("Socket ~p accepted\n", [Socket]),
             loop(Socket);
@@ -86,10 +82,10 @@ loop(Sock) ->
         {broadcast, Data} ->
             gen_tcp:send(Sock, Data),
             loop(Sock);
-        {tcp, Socket, Data} ->
-            io:format("Got packet: ~p\n", [Data]),
-            gen_tcp:send(Socket, Data),
-            loop(Socket);
+        {tcp, Socket, _Data} ->
+            io:format("socket ~p: incoming communication, aborting connection!", [Socket]),
+            gen_tcp:closed(Socket),
+            exit("incoming communication on write-only port!");
         {tcp_closed, Socket} ->
             io:format("Socket ~p closed\n", [Socket]);
         {tcp_error, Socket, Reason} ->
